35fa4639727bafb693d1c2093367594dab10ebbb
Christian Dean <chdprimary@gmail.com>
2017-10-09 22:38:00 -0500

    Alien Invasion - Startup and Ship
    Added:
    1) bootstrap code for the game window
    2) player ship now appears

---
+++ crash-course-book/alien-invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+import game_functions as gf
+
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, 
+        ai_settings.screen_height
+    ))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
+    
+    # Start the main loop for the game
+    while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, shipx)
+
+run_game()
