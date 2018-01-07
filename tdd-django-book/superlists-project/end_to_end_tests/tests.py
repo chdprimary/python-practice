98f9c8a7e45f408de7bad875e6f363c88e5fcbd2
Christian Dean <chdprimary@gmail.com>
2018-01-06 18:58:48 -0600

    New URL, view, and template for displaying lists

--- tdd-django-book/superlists-project/end_to_end_tests/tests.py
+++ tdd-django-book/superlists-project/end_to_end_tests/tests.py
@@ -87,7 +87,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         # Bob begins his own list.
-        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
