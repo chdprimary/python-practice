ce2e937c6e84ffcc4b746153ce114f5919fafcb3
Christian Dean <chdprimary@gmail.com>
2018-01-07 15:16:56 -0600

    Lists now have own unique URL

--- tdd-django-book/superlists-project/lists/tests.py
+++ tdd-django-book/superlists-project/lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Clean Code by Robert "Uncle Bob" Martin says:
 # Use TDD workflow, and tests should cover all ocde functionality
@@ -26,29 +26,47 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
-        response = self.client.get('/lists/the-only-list/')
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='Item 1', list=correct_list)
+        Item.objects.create(text='Item 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text="Other list item", list=other_list)
+        Item.objects.create(text="Other list item 2", list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
         self.assertContains(response, 'Item 1')
         self.assertContains(response, 'Item 2')
+        self.assertNotContains(response, 'Other list item')
+        self.assertNotContains(response, 'Other list item 2')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertTrue(saved_items.count(), 2)
@@ -56,4 +74,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
