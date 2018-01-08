15ff139e48f434c7722489baf189c68bd76d9f57
Christian Dean <chdprimary@gmail.com>
2018-01-07 18:15:46 -0600

    Can now add list items to an existing list

--- tdd-django-book/superlists-project/lists/tests.py
+++ tdd-django-book/superlists-project/lists/tests.py
@@ -29,6 +29,33 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -49,6 +76,12 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'Item 2')
         self.assertNotContains(response, 'Other list item')
         self.assertNotContains(response, 'Other list item 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
 class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
