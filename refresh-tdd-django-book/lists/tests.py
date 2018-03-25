8d9b6ffdaec993665103447acbd19efffb5ae84d
Christian Dean <chdprimary@gmail.com>
2018-03-25 15:57:21 -0500

    Display single to-do in to-do list

--- refresh-tdd-django-book/lists/tests.py
+++ refresh-tdd-django-book/lists/tests.py
@@ -5,3 +5,8 @@ class HomePageTest(TestCase):
         # this 'self.client' is the *django test client*
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_view_can_handle_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('1: A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
