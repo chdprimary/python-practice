2173b5546891c06104b5fedfb0d30efa7b6a0ae9
Christian Dean <chdprimary@gmail.com>
2018-03-25 21:39:37 -0500

    New URL, view, and template to display lists

--- refresh-tdd-django-book/e2e_tests/tests.py
+++ refresh-tdd-django-book/e2e_tests/tests.py
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Buy milk')
         input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('Buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Bob gets his own unique URL
         bob_list_url = self.browser.current_url
