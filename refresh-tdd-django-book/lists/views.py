0c2f43458f945626c37fdf26f2aff379f7429e41
Christian Dean <chdprimary@gmail.com>
2018-03-25 23:16:36 -0500

    New URL & views -- POST /lists/new

--- refresh-tdd-django-book/lists/views.py
+++ refresh-tdd-django-book/lists/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
+    return render(request, 'home.html')
+
+def new_list(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()
