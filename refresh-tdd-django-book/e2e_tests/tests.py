1744c69c2b5c4b755214cd444c35df1c93ece017
Christian Dean <chdprimary@gmail.com>
2018-03-25 20:43:13 -0500

    Test for multiple lists feature in e2e test

--- refresh-tdd-django-book/e2e_tests/tests.py
+++ refresh-tdd-django-book/e2e_tests/tests.py
@@ -1,6 +1,8 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 import time
 
 MAX_WAIT = 5
@@ -26,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.1)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Alice has heard about a cool new online todo app. She checks it out.
         self.browser.get(self.live_server_url)
 
@@ -64,11 +66,46 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Alice wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Alice starts a new to-do list
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        alice_list_url = self.browser.current_url
+        self.assertRegex(alice_list_url, '/lists/.+')
+
+        # A new user Bob comes along to the site.
+        ## We use a new browser session to make sure that no information
+        ## of Alice's is coming through from cookies, etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Bob visits the home page. There's no sign of Alice's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Bob starts a new list by entering a new item.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('Buy milk')
+
+        # Bob gets his own unique URL
+        bob_list_url = self.browser.current_url
+        self.assertRegex(bob_list_url, '/lists/.+')
+        self.assertNotEqual(bob_list_url, alice_list_url)
+
+        # Again, there is no trace of Alice's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
